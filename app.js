e1932e3503f9af4da4178b132f73e9e517d16412
MariamAbala <Mariamabala99@gmail.com>
2022-03-22 17:40:46 +0100

    setup project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+const { MONGOURI } = require('./keys')
+const mongoose = require('mongoose')
+
+
+
+//connect to database
+mongoose.connect(MONGOURI)
+
+// DB Connected
+mongoose.connection.on('connected',()=>{
+    console.log('MongoDB is connected successfully')
+})
+
+//DB connection Failed
+mongoose.connection.on('error',(err)=>{
+    console.log('MongoDB connection failed',console.err)
+})
+
+app.get('/',(req,res)=>{
+    res.send('Test Technique StrategIn')
+})
+
+app.listen(PORT,()=>{
+    console.log('-- Server is Running on ',PORT)
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:'mongodb+srv://mariam:Jgp89ij6xu9ewpiq@cluster0.fvo2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
