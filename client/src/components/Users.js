e796d651c4e1c1c7071d63290544a67249204e2d
MariamAbala <Mariamabala99@gmail.com>
2022-03-24 19:01:00 +0100

    users's list after login

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,23 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {useNavigate} from 'react-router-dom'
+
 
 const Users = () => {
+    const [data, setData] = useState([])
+    const [msg, setMsg] = useState("")
+    const [islogged,setLogged]=useState(false)
+    const navigate = useNavigate()
+    const setLoggedIn= () => {
+        setLogged(true)
+    }
+    useEffect(() => {
+        fetch('http://localhost:4000/users', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('jwt')
+            }
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result)
+                if (!result.error) {
+                    setData(result.users)
+                    setLoggedIn()
+                    
+                } else {
+                    setMsg(result)
+                    console.log('error' + msg.error)
+                }
+                
+
+            })
+    }, [])
     return (
-        <div className='card-center table-pos'>
-            <h2>Liste des utilisateurs</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">id</th>
-                        <th scope="col">email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>mariamabala@gmail.com</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div>
+            { islogged && (
+                <div className='card-center table-pos'>
+                    <h2>Liste des utilisateurs</h2>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map(item => {
+                                    return (
+    
+                                        <tr>
+                                            <td>{item.email}</td>
+                                        </tr>
+    
+                                    )
+                                })
+                            }             
+                        </tbody>
+                    </table>
+                    <button className='btn btn-dark' onClick={()=>{localStorage.clear(); navigate('/login')}}>Logout</button>
+                </div>
+            )}
+            {
+                !islogged && (
+                    <h2 className='label-color'>{msg.error}</h2>
+
+                )
+            }
+           
         </div>
     )
 }
